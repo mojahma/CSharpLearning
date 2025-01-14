8f13e7b833978b7152297f5b8c82e9a37f0a535b
mojahma <benkahugu@gmail.com>
2025-01-14 21:33:42 +0300

    Hello world

---
+++ hello world 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, Welcome to C# Day 1");
+        }
+    }
+}
